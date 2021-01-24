0dd9b151ebb869d1a29ae81d7e16ff866c4c262d
k.trolp@gmx.at <nii@gmx.at>
2021-01-24 11:01:59 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'security-alert adapter settings': {
+		'en': 'Adapter settings for security-alert',
+		'de': 'Adaptereinstellungen für security-alert',
+		'ru': 'Настройки адаптера для security-alert',
+		'pt': 'Configurações do adaptador para security-alert',
+		'nl': 'Adapterinstellingen voor security-alert',
+		'fr': "Paramètres d'adaptateur pour security-alert",
+		'it': "Impostazioni dell'adattatore per security-alert",
+		'es': 'Ajustes del adaptador para security-alert',
+		'pl': 'Ustawienia adaptera dla security-alert',
+		'zh-cn': 'security-alert的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
